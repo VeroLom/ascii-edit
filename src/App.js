149d2a5760416392b86dcdb0d0b8cfe560b0cd12
VeroLom <ver@0xff.su>
2024-01-05 23:54:16 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import DrawingTable from "./components/DrawingTable";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>ASCII Editor</h1>
+            </header>
+
+            <main>
+
+                <aside>
+                </aside>
+
+                <section>
+                    <DrawingTable />
+                </section>
+            </main>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/DrawingTable.js
@@ -0,0 +1,9 @@
+function DrawingTable() {
+    const initialTableData = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+    ]
+}
+
+export default DrawingTable;
