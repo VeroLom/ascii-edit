b255c7c3a1a5a65ec5e275ffd99778930c9d348a
VeroLom <ver@0xff.su>
2024-01-07 22:43:04 +0100

    Added insertMode toggle to the sidebar

--- src/components/SidebarInfo.js
+++ src/components/SidebarInfo.js
@@ -3,23 +3,36 @@ import useStore from "../store";
 import styles from "./SidebarInfo.module.css";
 
 const SidebarInfo = () => {
-    const {showGrid, setShowGrid, insertMode, selectedCell, getTableSize} = useStore();
+    const {showGrid, setShowGrid, insertMode, setInsertMode, selectedCell, getTableSize} = useStore();
 
     function toggleShowGrid() {
         setShowGrid(!showGrid);
+    }
+
+    function toggleInsertMode() {
+        setInsertMode(!insertMode);
     }
 
     return (
         <div className={styles.info}>
             <p>Size: {getTableSize()[0]}x{getTableSize()[1]}</p>
             <p>Coord: {selectedCell[0] + 1}:{selectedCell[1] + 1}</p>
+
             <p>Grid:&nbsp;
                 <span className={`${styles.toggle} ${showGrid ? styles.on : ''}`}
                       onClick={() => toggleShowGrid()}
                 >
-                {showGrid ? 'ON' : 'OFF'}</span>
+                    {showGrid ? 'ON' : 'OFF'}
+                </span>
             </p>
-            <p>Insert: {insertMode ? 'ON' : 'OFF'}</p>
+
+            <p>Insert:&nbsp;
+                <span className={`${styles.toggle} ${insertMode ? styles.on : ''}`}
+                      onClick={() => toggleInsertMode()}
+                >
+                    {insertMode ? 'ON' : 'OFF'}
+                </span>
+            </p>
         </div>
     );
 };
